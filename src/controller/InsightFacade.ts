c874066391021090cb4ae31500060cd5fa6c5e5f
Xiaojing <jojoxiaojing@icloud.com>
2017-10-02 02:52:52 -0700

    Finish data parse

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -2,17 +2,69 @@
  * This is the main programmatic entry point for the project.
  */
 import {IInsightFacade, InsightResponse} from "./IInsightFacade";
-
 import Log from "../Util";
+var JSZip = require("jszip");
+let dataStore:Course[] = [];
+
+interface Course {
+    courses_dept: string;
+    courses_id: string;
+    courses_avg: number;
+    courses_instructor: string;
+    courses_title: string;
+    courses_pass: number;
+    courses_fail: number;
+    courses_audit: number;
+    courses_uuid: string;
+}
 
 export default class InsightFacade implements IInsightFacade {
-
+    getValue() {
+        return dataStore;
+    }
     constructor() {
         Log.trace('InsightFacadeImpl::init()');
     }
 
     addDataset(id: string, content: string): Promise<InsightResponse> {
-        return null;
+        return new Promise<InsightResponse>((fullfill, reject) =>{
+            JSZip.loadAsync(content, {base64: true}).then(function (zip:any) {
+                let promiseArr:Array<Promise<any>> = new Array();
+                let JSONarr:Array<any> = new Array();
+                let parseResult:any[] = new Array();
+                for(let key in zip.files){
+                    if (zip.file(key)) {
+                        let contentInFIle = zip.file(key).async("string");
+                        promiseArr.push(contentInFIle);
+                    }}
+                Promise.all(promiseArr).then(function(value:any){
+
+                     for (let i of value){
+                         try{
+                             let m = JSON.parse(i);
+                             parseResult.push(m);
+                         }
+                         catch(err){
+
+                         }
+
+                     }
+
+                    let s = {
+                        code: 1,
+                        body: {parseResult}
+                    };
+                    fullfill(
+                        s
+                    );
+                }).catch(function(err){
+
+                });
+
+            }).catch(function (err:any) {
+                reject(err);
+            });
+        });
     }
 
     removeDataset(id: string): Promise<InsightResponse> {
--- test/EchoSpec.ts
+++ test/EchoSpec.ts
@@ -6,22 +6,80 @@ import Server from "../src/rest/Server";
 import {expect} from 'chai';
 import Log from "../src/Util";
 import {InsightResponse} from "../src/controller/IInsightFacade";
+import InsightFacade from "../src/controller/InsightFacade";
+var fs = require("fs");
 
-describe("EchoSpec", function () {
+
+// describe("EchoSpec", function () {
+//
+//
+//     function sanityCheck(response: InsightResponse) {
+//         expect(response).to.have.property('code');
+//         expect(response).to.have.property('body');
+//         expect(response.code).to.be.a('number');
+//     }
+//
+//     before(function () {
+//         Log.test('Before: ' + (<any>this).test.parent.title);
+//     });
+//
+//     beforeEach(function () {
+//         Log.test('BeforeTest: ' + (<any>this).currentTest.title);
+//     });
+//
+//     after(function () {
+//         Log.test('After: ' + (<any>this).test.parent.title);
+//     });
+//
+//     afterEach(function () {
+//         Log.test('AfterTest: ' + (<any>this).currentTest.title);
+//     });
+//
+//     it("Should be able to echo", function () {
+//         let out = Server.performEcho('echo');
+//         Log.test(JSON.stringify(out));
+//         sanityCheck(out);
+//         expect(out.code).to.equal(200);
+//         expect(out.body).to.deep.equal({message: 'echo...echo'});
+//     });
+//
+//     it("Should be able to echo silence", function () {
+//         let out = Server.performEcho('');
+//         Log.test(JSON.stringify(out));
+//         sanityCheck(out);
+//         expect(out.code).to.equal(200);
+//         expect(out.body).to.deep.equal({message: '...'});
+//     });
+//
+//     it("Should be able to handle a missing echo message sensibly", function () {
+//         let out = Server.performEcho(undefined);
+//         Log.test(JSON.stringify(out));
+//         sanityCheck(out);
+//         expect(out.code).to.equal(400);
+//         expect(out.body).to.deep.equal({error: 'Message not provided'});
+//     });
+//
+//     it("Should be able to handle a null echo message sensibly", function () {
+//         let out = Server.performEcho(null);
+//         Log.test(JSON.stringify(out));
+//         sanityCheck(out);
+//         expect(out.code).to.equal(400);
+//         expect(out.body).to.have.property('error');
+//         expect(out.body).to.deep.equal({error: 'Message not provided'});
+//     });
+//
+// });
 
 
-    function sanityCheck(response: InsightResponse) {
-        expect(response).to.have.property('code');
-        expect(response).to.have.property('body');
-        expect(response.code).to.be.a('number');
-    }
-
+describe("testAddData", function(){
+    var insightF:InsightFacade;
     before(function () {
         Log.test('Before: ' + (<any>this).test.parent.title);
     });
 
     beforeEach(function () {
         Log.test('BeforeTest: ' + (<any>this).currentTest.title);
+        insightF = new InsightFacade();
     });
 
     after(function () {
@@ -32,37 +90,24 @@ describe("EchoSpec", function () {
         Log.test('AfterTest: ' + (<any>this).currentTest.title);
     });
 
-    it("Should be able to echo", function () {
-        let out = Server.performEcho('echo');
-        Log.test(JSON.stringify(out));
-        sanityCheck(out);
-        expect(out.code).to.equal(200);
-        expect(out.body).to.deep.equal({message: 'echo...echo'});
-    });
+    it("Import course.zip and store the data", function(done){
 
-    it("Should be able to echo silence", function () {
-        let out = Server.performEcho('');
-        Log.test(JSON.stringify(out));
-        sanityCheck(out);
-        expect(out.code).to.equal(200);
-        expect(out.body).to.deep.equal({message: '...'});
-    });
+        fs.readFile(__dirname + '/data/courses.zip', "base64", function(err:any, data:string) {
 
-    it("Should be able to handle a missing echo message sensibly", function () {
-        let out = Server.performEcho(undefined);
-        Log.test(JSON.stringify(out));
-        sanityCheck(out);
-        expect(out.code).to.equal(400);
-        expect(out.body).to.deep.equal({error: 'Message not provided'});
-    });
+            insightF.addDataset("Courses",data).then(function(zip:any){
+                    let mm = zip["body"];
 
-    it("Should be able to handle a null echo message sensibly", function () {
-        let out = Server.performEcho(null);
-        Log.test(JSON.stringify(out));
-        sanityCheck(out);
-        expect(out.code).to.equal(400);
-        expect(out.body).to.have.property('error');
-        expect(out.body).to.deep.equal({error: 'Message not provided'});
-    });
 
+            }).catch();
+
+
+
+
+
+
+
+
+        });
+
+    });
 });
