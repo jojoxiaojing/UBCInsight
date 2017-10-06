fdd5f4fc4d0ea161cd6f7be705d556c5d81fbbce
Andrew Bolyachevets <andrew.bolyachevets@utoronto.ca>
2017-10-05 17:10:22 -0700

    more console log in starter files

--- src/App.ts
+++ src/App.ts
@@ -9,18 +9,18 @@ import Log from './Util';
  */
 export class App {
     public initServer(port: number) {
-        Log.info('App::initServer( ' + port + ' ) - start');
+        //Log.info('App::initServer( ' + port + ' ) - start');
 
         let s = new Server(port);
         s.start().then(function (val: boolean) {
-            Log.info("App::initServer() - started: " + val);
+            //Log.info("App::initServer() - started: " + val);
         }).catch(function (err: Error) {
-            Log.error("App::initServer() - ERROR: " + err.message);
+            //Log.error("App::initServer() - ERROR: " + err.message);
         });
     }
 }
 
 // This ends up starting the whole system and listens on a hardcoded port (4321)
-Log.info('App - starting');
+//Log.info('App - starting');
 let app = new App();
 app.initServer(4321);
