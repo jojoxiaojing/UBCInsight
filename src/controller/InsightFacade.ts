c5308b21e52256b12c1074b0483e3d575ac1fcaa
Xiaojing <jojoxiaojing@icloud.com>
2017-10-09 06:32:32 -0700

    Fix removedataset and check data invalidity

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -45,58 +45,75 @@ export default class InsightFacade implements IInsightFacade {
                     }}
                 //Log.trace("101:Begin promise all");
 
-                Promise.all(promiseArr).then(function(value:any){
-                    //Log.trace("120:Begin json parse the data");
 
-                    for (let i of value){
-                        try{
-                            let m = JSON.parse(i);
-                            parseResult.push(m);
-                        }
-                        catch(err){
-                            //do nothing here
-                        }
-                    }
-                    //Log.trace("130:Begin to transform the data into Course Object");
-                    
-                    for (let i of parseResult){
-                        let courseData:Array<any> = i.result;
-                        for(let c of courseData){
-                            let m:Course = {
-                                courses_dept: c.Subject,
-                                courses_id: c.Course,
-                                courses_avg: c.Avg,
-                                courses_instructor: c.Professor,
-                                courses_title: c.Title,
-                                courses_pass: c.Pass,
-                                courses_fail: c.Fail,
-                                courses_audit: c.Audit,
-                                courses_uuid: c.id
-                            };
-                            dataInMemory.data.push(m);
-                        }
-                    }
 
-                    //Log.trace("140:Begin returning InsightResponse");
-                    //decide return 201 or 204
-                    let c;
-                    if(id == dataInMemory.id){
-                        c = 201;
-                    }else{
-                        c = 204;
-                    }
-                    dataInMemory.id =id;
+                if(promiseArr.length !== 0){
+                    Promise.all(promiseArr).then(function(value:any){
+                        //Log.trace("120:Begin json parse the data");
+
+                        for (let i of value){
+                            try{
+                                let m = JSON.parse(i);
+                                parseResult.push(m);
+                            }
+                            catch(err){
+                                //do nothing here
+                            }
+                        }
+                        //Log.trace("130:Begin to transform the data into Course Object");
+
+                        for (let i of parseResult){
+                            let courseData:Array<any> = i.result;
+                            for(let c of courseData){
+                                let m:Course = {
+                                    courses_dept: c.Subject,
+                                    courses_id: c.Course,
+                                    courses_avg: c.Avg,
+                                    courses_instructor: c.Professor,
+                                    courses_title: c.Title,
+                                    courses_pass: c.Pass,
+                                    courses_fail: c.Fail,
+                                    courses_audit: c.Audit,
+                                    courses_uuid: c.id
+                                };
+                                dataInMemory.data.push(m);
+                            }
+                        }
+
+                        //Log.trace("140:Begin returning InsightResponse");
+                        //decide return 201 or 204
+                        let c;
+
+
+                        if(id == dataInMemory.id){
+                            c = 201;
+                        }else{
+                            c = 204;
+                        }
+                        dataInMemory.id =id;
+                        let s:InsightResponse = {
+                            code: c,
+                            body: {dataStore: dataInMemory}
+                        };
+                        //store the data into data/data.json
+                        // Log.trace(__dirname);
+                        fs.writeFileSync(__dirname + '/data.txt', JSON.stringify(dataInMemory), 'utf-8');
+                        fullfill(s);
+
+
+                    }).catch(function(err:any){
+                        let a = err;
+                        throw new Error(a.message);
+                    });
+                }else{
                     let s:InsightResponse = {
-                        code: c,
-                        body: {dataStore: dataInMemory}
+                        code: 400,
+                        body: {"Error": "Dataset is invalid"}
                     };
-                    //store the data into data/data.json
-                   // Log.trace(__dirname);
-                    fs.writeFileSync(__dirname + '/data.txt', JSON.stringify(dataInMemory), 'utf-8');
-                    fullfill(s);
-                }).catch(function(err){
-                    throw new Error(err);
-                });
+                    reject(s);
+                }
+
+
 
 
             }).catch(function (err:any) {
@@ -125,14 +142,15 @@ export default class InsightFacade implements IInsightFacade {
                             dataInMemory.data = [];
                             fs.unlink(__dirname + '/data.txt');
                             s.code = 204;
+                            s.body = {message:"Remove data successfully"};
                             fullfill(s);
                         } else {
-                            s.code = 400;
+                            s.code = 404;
                             reject(s);
                         }
                     });
                 }else{
-                    s.code = 400;
+                    s.code = 404;
                     reject(s);
                 }
             }else if(dataInMemory.id == id){
@@ -144,7 +162,7 @@ export default class InsightFacade implements IInsightFacade {
                 s.code = 204;
                 fullfill(s);
             }else{
-                s.code = 400;
+                s.code = 404;
                 reject(s);
             }
 
--- test/AddDataset.ts
+++ test/AddDataset.ts
@@ -2,6 +2,7 @@
 import {expect} from 'chai';
 import Log from "../src/Util";
 import InsightFacade from "../src/controller/InsightFacade";
+import {InsightResponse} from "../src/controller/IInsightFacade";
 var fs = require("fs");
 
 
@@ -25,43 +26,106 @@ describe("testAddData", function() {
         //Log.test('AfterTest: ' + (<any>this).currentTest.title);
     });
 
-    it("Import course.zip and store the data, it should return code 204", function (done) {
-
+    it("Import course.zip ，store the data and remove successfully", function (done) {
 
         fs.readFile(__dirname + '/data/courses.zip', "base64", function (err: any, data: string) {
 
-            insightF.addDataset("Courses", data).then(function (value: any) {
+            insightF.addDataset("Courses", data).then(function (value: InsightResponse) {
                 let a = value;
                 expect(a.code).to.deep.equal(204);
-                var testQuery = {"WHERE": {GT: {courses_audit: 10}}}
-                insightF.performQuery(testQuery).then(function (value: any) {
-                    let a = value;
+                let m = fs.existsSync('./src/controller/data.txt');
+                expect(m).to.deep.equal(true);
+                let dataID = insightF.getValue().id;
+                expect(dataID).to.deep.equal("Courses");
+                insightF.removeDataset("Courses").then(function (value: InsightResponse) {
+                    let m = value;
+                    expect(m.code).to.deep.equal(204);
+                    let ifFileExist = fs.existsSync('./src/controller/data.txt');
+                    expect(ifFileExist).to.deep.equal(false);
                     done()
-                }).catch(function (err) {
-                    //log.error("Error: " + err);
+                }).catch(function (value: InsightResponse) {
                     expect.fail();
                     done()
                 });
+
+            }).catch(function (err: InsightResponse) {
+
             });
         });
     });
 
-    it("Import empty.zip and store the data, it should return code 400", function (done) {
+    it("Import course.zip ，store the data and remove unsuccessfully", function (done) {
 
+        fs.readFile(__dirname + '/data/courses.zip', "base64", function (err: any, data: string) {
 
-        fs.readFile(__dirname + '/data/empty.zip', "base64", function (err: any, data: string) {
-
-            insightF.addDataset("Empty", data).then(function (value: any) {
-                    let a = value;
-                    expect(a.code).to.deep.equal(400);
-                    done()
-                }).catch(function (err) {
-                    Log.error("Error: " + err);
+            insightF.addDataset("Courses", data).then(function (value: InsightResponse) {
+                let a = value;
+                expect(a.code).to.deep.equal(204);
+                let m = fs.existsSync('./src/controller/data.txt');
+                expect(m).to.deep.equal(true);
+                let dataID = insightF.getValue().id;
+                expect(dataID).to.deep.equal("Courses");
+                insightF.removeDataset("Empty").then(function (value: InsightResponse) {
                     expect.fail();
                     done()
+                }).catch(function (value: InsightResponse) {
+                    let m = value;
+                    expect(m.code).to.deep.equal(404);
+                    let ifFileExist = fs.existsSync('./src/controller/data.txt');
+                    expect(ifFileExist).to.deep.equal(true);
+                    done()
                 });
+            }).catch(function (err: InsightResponse) {
             });
         });
+    });
 
+    it("Import empty.zip, it should return code 400", function (done) {
+        fs.readFile(__dirname + '/data/emptyFolder.zip', "base64", function (err: any, data: string) {
 
+            insightF.addDataset("Empty", data).then(function (value: InsightResponse) {
+
+                expect.fail();
+                done()
+            }).catch(function (err: InsightResponse) {
+                let a = err;
+                expect(a.code).to.deep.equal(400);
+                let ifFileExist = fs.existsSync('./src/controller/data.txt');
+                expect(ifFileExist).to.deep.equal(false);
+                done()
+            });
+        });
+    });
+
+    it("Import empty.zip, it should return code 400", function (done) {
+        fs.readFile(__dirname + '/data/emptyFolder.zip', "base64", function (err: any, data: string) {
+
+            insightF.addDataset("Empty", data).then(function (value: InsightResponse) {
+
+                expect.fail();
+                done()
+            }).catch(function (err: InsightResponse) {
+                let a = err;
+                expect(a.code).to.deep.equal(400);
+                let ifFileExist = fs.existsSync('./src/controller/data.txt');
+                expect(ifFileExist).to.deep.equal(false);
+                done()
+            });
+        });
+    });
+
+    it("Given an invalid string and return 400", function (done) {
+
+        insightF.addDataset("Empty", "A").then(function (value: InsightResponse) {
+
+            expect.fail();
+            done()
+        }).catch(function (err: InsightResponse) {
+            let a = err;
+            expect(a.code).to.deep.equal(400);
+            let ifFileExist = fs.existsSync('./src/controller/data.txt');
+            expect(ifFileExist).to.deep.equal(false);
+            done()
+        });
+    });
 })
